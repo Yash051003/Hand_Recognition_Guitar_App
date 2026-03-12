5a16b267a41cbbe167e54e6da41e2aa92e0cbea8
Yash Taank <22bee122@nith.ac.in>
2026-03-13 00:34:48 +0530

    checking

--- streamapp/routing.py
+++ streamapp/routing.py
@@ -1,6 +1,6 @@
 from django.urls import re_path
-from . import consumers
+from streamapp import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/stream/', consumers.StreamConsumer.as_asgi()),
+    re_path(r'ws/stream/$', consumers.StreamConsumer.as_asgi()),
 ]
